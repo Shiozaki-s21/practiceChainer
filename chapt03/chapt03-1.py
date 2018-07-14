a69660182277af6ebbbde992cc9e007e53dadd8e
Subaru <shiozaki5621@gmail.com>
2018-07-14 00:58:59 -0700

    finish chapt03 - 1

--- chapt03/chapt03-1.py
+++ chapt03/chapt03-1.py
@@ -40,12 +40,61 @@ while len(next_page) > 0:
     with urllib.request.urlopen(url) as response:
         html = response.read().decode('utf-8')
 
-        title - re.findall(r'<title>([¥s¥S]*)) - Wikimedeia Commons</title>', html)
+        title = re.findall(r'<title>([\s\S]*) - Wikimedia Commons</title>', html)
 
-        if lem(title) < 1 :
+        if len(title) < 1 :
             break
 
-        nextpage = re.findall(¥r'<a¥s*href=¥"(/w/index.php?[¥s¥S]*)¥" title=¥"' + title[0] +'¥">[¥s¥S]*>next page</a>',¥html)
+        nextpage = re.findall(\
+				r'<a\s*href=\"(/w/index.php?[\s\S]*)\" title=\"'+title[0]+'\">[\s\S]*>next page</a>',\
+				html)
 
+        #ギャラリー表示部分のタグを取得する
+        gallery = re.findall(\
+			r'<div class=\"gallerytext\">\s+<a\s+href=\"/wiki/File:(\S*)\"',\
+			html, re.DOTALL)
 
-        #re.findall(¥r'<div class = ¥"gallerytext¥">¥s + <a¥s + href = ¥"/wiki/File:(¥S*)¥"',¥html, re .DOTALL>)
+        #ギャラリーを開く
+        for g in gallery:
+            #サブページを開く
+            with urllib.request.urlopen(suburl + g) as response:
+                g = urllib.parse.quote_plus(urllib.parse.unquote_plus(g))
+
+                #URLから読み込む
+                html = response.read().decode('utf-8')
+                original = re.findall(\
+					r'<a\s+(?:class=\"internal\")?\s*href=\"(https://upload.wikimedia.org/\S*/'+g+')\"[\s\S]*>[\s\S]*</a>',\
+					html)
+
+                #画像をダウンロード
+                for o in original:
+                    face = o.rsplit('/', 1)[1]
+                    os.system('wget '+o+' -O portrait/'+face)
+                    #TIFファイルはJpegに変換する
+                    if face.endswith('.tif') or face.endswith('.tiff'):
+                        os.system('gdal_translate -of JPEG -ot Byte -co QUALITY=100 portrait/'+face+' portrait/'+face+'.jpg')
+                        os.remove('portrait/' + face)
+                        os.remove('portrait/' + face + '.jpg.aux.xml')
+
+        #次のページのURLを作る
+        if len(nextpage) > 0:
+            next = nextpage[0].replace('&amp;','&')
+            next_page = base_url+ next
+        else:
+            next_page = ''
+
+        #ダウンロードした画像を全て読み込み、画像の上部から正方形を切りだす
+        fs = os.listdir('portrait')
+        numimg = 0
+
+        for fn in  fs:
+            #画像読み込み
+            img = Image.open('portrait/' + fn).convert('RGB')
+            #上部中央の顔付近を切り出す
+            w = img.size[0] / 2
+            x = img.size[0] / 4
+            img = img.crop((x, 0, x + w, w).resize(320, 320))
+
+            #名前をつけて保存する
+            img.save('train/' + str(numimg) + '.png')
+            numimg = numimg + 1
