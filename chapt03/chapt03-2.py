c7d23a3fed2d6c05733b573f205de75147896d33
Subaru <shiozaki5621@gmail.com>
2018-07-14 13:29:00 -0700

    add chpt03-2

---
+++ chapt03/chapt03-2.py
@@ -0,0 +1,14 @@
+import chainer
+import chainer.functions as func #F
+import chainer.links as links #L
+from   chainer import training, datasets, iterators, optimizers
+from   chainer.training import extensions
+import numpy as np
+
+import codecs
+import re
+import urllib.parse
+import urllib.request
+import os
+import socket
+from PIL import Image
